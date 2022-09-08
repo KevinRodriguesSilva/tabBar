b36a89dbcbb4243d37ef475353aa76e80a4ed7bc
KevinRodriguesSilva <kevin.ponas@gmail.com>
2022-09-08 10:45:33 -0300

    inserção dos icones

--- App.js
+++ App.js
@@ -6,20 +6,28 @@ import Home from "./src/telas/Home";
 import Contato from "./src/telas/Contato";
 import Sobre from "./src/telas/Sobre";
 
+import { Entypo } from '@expo/vector-icons';
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-          <Tab.Screen name="home" component={Home} />
-          {/* component = {Home}
+          <Tab.Screen name="home" component={Home}
           options = {{
-          //   tabBarIcon : ({color, size}) => {
-          //     return <Entypo name="home" size={size} color={color} />
-          //   }
-          }} */}
-          <Tab.Screen name="contato" component={Contato} />
+             tabBarIcon : ({color, size}) => {
+               return <Entypo name="home" size={size} color={color} />
+             }
+          }}
+          />
+          <Tab.Screen name="contato" component={Contato} 
+          options = {{
+            tabBarIcon : ({color, size}) => {
+              return <Entypo name="bell" size={size} color={color} />
+            }
+          }}
+          />
           <Tab.Screen name="sobre" component={Sobre} />
       </Tab.Navigator>
     </NavigationContainer>
